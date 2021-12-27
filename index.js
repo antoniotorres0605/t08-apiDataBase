1124fa5d2069824e6b92040b8ffaffae9a0c81c1
antoniotorres0605 <jantonio.torresfi@gmail.com>
2021-12-27 00:27:48 -0600

    Clase con DB funcional

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//@Autor: Antonio Torres
+//@Fecha: 27/12/2021
+
+
+//Dependencias
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,144 +12,61 @@ let path = require("path");
 // Rutas
 let {Router} = express;
 let router = new Router();
-let id = new Router();
+//let id = new Router();
 
-
-// Clases
-
-let Contenedor = require("./contenedor");
-let c1 = new Contenedor('./productos.txt');
+//Base de datos
+const db_obj= require("./config/db");
+const db = db_obj.client;
 
 // Puerto
 const PORT = config.port;
 
-const db_obj= require("./config/db");
-const db = db_obj.client;
-
-
+// Clases
+let Contenedor = require("./contenedorDB");
+let c1 = new Contenedor('items',db);
 
 // Middlewares
 app.use(cors(config.cors));
 
 
 // Settings
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-/* 
-router.get("/",(req,res,next)=>{
-  c1.getAll().then(data=>{
-    res.send(data);
-  }).catch(error=>{
-    res.send(error);
-  })
-});
+app.use(express.json());
 
 
+//Dirección de las rutas
 
-id.get("/:id",(req,res,next)=>{
-  console.log("Entre al get")
+// RUTA localhost/api
+router.get("/:id?",(req,res,next)=>{
   let id = (req.params.id);
-  console.log(id)
-  c1.getById(id).then(data=>{
-    console.log(data);
-    res.json(data);
-  }).catch(error=>{
-    res.send(error);
-  });
-});
-
-router.post("/",(req,res,next)=>{
-  console.log(req.body.producto);
-  c1.save(req.body.producto).then(data=>{
-    console.log(data);
-    res.json(data);
-  }).catch(error=>{
-    res.send(error);
-  })
-});
-
-*/
-
-
-// CREACION DE TABLA
-(async ()=>{
-  try {
-    await db.schema.createTable("articulos", table=>{
-      table.increments("id").primary(),
-      table.string("nombre"),
-      table.string("codigo"),
-      table.float("precio"),
-      table.integer("stock")
-    })
-  } catch (error) {
-    console.log(error)
-    
+  if(id == undefined){
+    c1.getAll().then(data=>{
+      //res.render("historial1",{data});
+      res.send(data);
+    }).catch(error=>{
+      res.send(error);
+    });
+  }
+  else{
+    c1.getById(id).then(datos=>{
+      let data = [];
+      data.push(datos);
+      //res.render("historial1",{data});
+      res.send(datos);
+    }).catch(error=>{
+      res.send(error);
+    });
   }
 });
-
-// INSERTAR EN TABLA
-(async ()=>{
-  try {
-    let data = [
-      {
-        nombre:"Televisor",
-        codigo:"cd123",
-        precio:"12.5",
-        stock:10
-      }
-    ];
-    let response = await db.from("articulos").insert(data);
-    console.log(response);
-  } catch (error) {
-    console.log(error)
-    
-  }
-});
-
-// MOSTRAR EN TABLA
-(async ()=>{
-  try {
-    let response = await db.from("articulos");
-    console.log(response);
-  } catch (error) {
-    console.log(error)
-    
-  }
-});
-
-// ELIMINAR EN TABLA
-(async ()=>{
-  try {
-    let response = await db.from("articulos").where("id","=",2).del();
-    console.log(response);
-  } catch (error) {
-    console.log(error)
-    
-  }
-});
-
-// ACTUALIZAR DATOS
-(async ()=>{
-  try {
-    let response = await db.from("articulos").where("id","=",1).update({stock:100});
-    console.log(response);
-  } catch (error) {
-    console.log(error)
-    
-  }
-})();
-
-
 
 app.use("/api/productos",router);
-app.use("/api/productos",id);
-app.use("/api",express.static(path.join(__dirname,"public","html")));
 
+//Ruta raiz
 app.get("/", (req,res,next) => {
   res.send("<h1>Pagina de Inicio<br></h1>");
 });
 
+//Ruta 
 app.listen(PORT,()=>{
   console.log(`Conexión hecha en http://localhost:${PORT}`);
 });
